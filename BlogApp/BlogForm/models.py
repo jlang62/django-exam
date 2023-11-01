91708a263ec767ac8eae1579a84aca017e087c1d
jesselang <jesselang62@gmail.com>
2023-11-01 16:23:29 +0100

    adjustments

--- BlogApp/BlogForm/models.py
+++ BlogApp/BlogForm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BlogModel(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=True)
+    img = models.ImageField(upload_to="images/")
+    video = models.FileField(upload_to="videos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- BlogApp/BlogForm/views.py
+++ BlogApp/BlogForm/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import InputForm
+from .models import BlogModel
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def home_view(request):
+    context = {}
+
+    if request.method == 'POST':
+        form = InputForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            logger.info("Form saved successfully.")
+        else:
+            logger.error("Form is not valid. Errors: %s", form.errors)
+
+    items = BlogModel.objects.all()
+    context['form'] = InputForm()
+    context['items'] = items
+
+    return render(request, "form.html", context)
