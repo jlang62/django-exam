ddf5c601859e9f36e5f44d763cb479df72fb643a
jesselang <jesselang62@gmail.com>
2023-11-02 16:01:57 +0100

    dynamic routing

--- BlogApp/BlogOverview/urls.py
+++ BlogApp/BlogOverview/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('blogs/', views.home_view, name='blogs'),
+     path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
 ]
--- BlogApp/BlogOverview/views.py
+++ BlogApp/BlogOverview/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from BlogForm.models import BlogModel
 
@@ -9,3 +9,7 @@ def home_view(request):
     context['items'] = items
 
     return render(request, "blogs.html", context)
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(BlogModel, id=blog_id)
+    return render(request, 'blog_detail.html', {'blog': blog})
